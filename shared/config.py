91993f4f30dcfb27cff43e9a9eb805b6eb102040
NovaProtocol <novaprotocol@users.noreply.github.com>
2026-08-12 11:45:03 +0800

    remove dead DebugConfig and config_dict from shared config

--- shared/config.py
+++ shared/config.py
@@ -5,15 +5,5 @@ class BaseConfig:
     pass
 
 
-class DebugConfig(BaseConfig):
-    DEBUG = True
-
-
 class ProductionConfig(BaseConfig):
     DEBUG = False
-
-
-config_dict = {
-    "Debug": DebugConfig,
-    "Production": ProductionConfig,
-}
